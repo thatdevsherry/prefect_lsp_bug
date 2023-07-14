ac4b06c8a52ab80bfc0f8354e03fda61a3240683
Shehriyar Qureshi <m.shehriyar@abysssolutions.com.au>
2023-07-14 05:36:39 +0500

    add working example

--- example.py
+++ example.py
@@ -1,20 +1,21 @@
-from prefect import flow, task
-from typing import List
-import httpx
+from prefect import flow, task, get_run_logger
 from prefect.deployments import Deployment
+import httpx
 
 
 @task(retries=3)
-def get_stars(repo: str):
+def get_stars(repo: str) -> int:
     url = f"https://api.github.com/repos/{repo}"
     count = httpx.get(url).json()["stargazers_count"]
-    print(f"{repo} has {count} stars!")
+    return count
 
 
 @flow(name="GitHub Stars")
-def github_stars(repos: List[str]):
-    for repo in repos:
-        get_stars(repo)
+def github_stars():
+    repo = "thatdevsherry/historia"
+    logger = get_run_logger()
+    result = get_stars(repo)
+    logger.info(f"{repo} has {result} stars")
 
 
 deployment = Deployment.build_from_flow(
